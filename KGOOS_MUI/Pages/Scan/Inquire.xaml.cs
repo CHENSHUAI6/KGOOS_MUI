1b3ae20033ae9086fda47c6e7fcd12cda6d1aac8
CHENSHUAI6 <chenshuai@chenshuaideMacBook.local>
2018-03-06 16:31:04 +0800

    3-6
    到家扫描查询界面

--- KGOOS_MUI/Pages/Scan/Inquire.xaml.cs
+++ KGOOS_MUI/Pages/Scan/Inquire.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,63 @@ namespace KGOOS_MUI.Pages.Scan
         public Inquire()
         {
             InitializeComponent();
+        }
+        public void colorDG()
+        {
+
+            for (int i = 0; i < this.DG1.Items.Count; i++)
+            {
+                DataRowView drv = DG1.Items[i] as DataRowView;
+                DataGridRow row = (DataGridRow)this.DG1.ItemContainerGenerator.ContainerFromIndex(i);
+                if (i == 2)
+                {
+                    row.Background = new SolidColorBrush(Colors.Blue);
+                }
+            }
+        }
+        public void getData()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add(new DataColumn("FirstName", typeof(int)));
+            dt.Columns.Add(new DataColumn("LastName", typeof(string)));
+
+            for (int i = 0; i < 6; i++)
+            {
+                DataRow dr = dt.NewRow();
+                if (i == 3)
+                {
+                    dr["FirstName"] = DBNull.Value;
+                    dr["LastName"] = DBNull.Value;
+                    dt.Rows.Add(dr);
+                }
+                else
+                {
+                    dr["FirstName"] = i;
+                    dr["LastName"] = "tom" + i.ToString();
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            this.DG1.CanUserAddRows = false;
+            this.DG1.ItemsSource = dt.DefaultView;
+        }
+
+        //datagrid赋值
+        
+        private void DG1_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            var drv = e.Row.Item as DataRowView;
+            switch (drv["FirstName"].ToString())
+            {
+                case "1": e.Row.Background = new SolidColorBrush(Colors.Green);
+                    break;
+                case "2": e.Row.Background = new SolidColorBrush(Colors.Yellow);
+                    break;
+                case "3": e.Row.Background = new SolidColorBrush(Colors.CadetBlue);
+                    break;
+
+
+            }
         }
     }
 }
